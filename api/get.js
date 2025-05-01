0c4774ac584534de6349cc5f7a90f00601366070
badirnakid <badirnakid@gmail.com>
2025-05-01 14:24:54 -0600

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://app.triggui.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const url = "https://raw.githubusercontent.com/badirnakid/triggui-content/main/contador.json";
   const response = await fetch(url);
   const data = await response.json();
